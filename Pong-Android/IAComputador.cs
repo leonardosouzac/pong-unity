df97ac2a2bbcdce65d81b76aa59fea39bb82a80a
Leonardo Celso de Souza <leonardo_souzac@hotmail.com>
2020-04-07 10:58:30 -0300

    Add files via upload

---
+++ Pong-Android/IAComputador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAComputador : MonoBehaviour
+{
+    public Transform Ball;
+    public float velocidade;
+    private Rigidbody2D rbComp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbComp = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rbComp.velocity = new Vector2(0, (Ball.transform.position.y -  transform.position.y) * velocidade);
+
+    }
+}
